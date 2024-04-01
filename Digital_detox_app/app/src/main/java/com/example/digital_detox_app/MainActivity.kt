7766edd4eea51bcd1ca567cff144c306724f2dc1
john <ypoh007@e.ntu.edu.sg>
2024-04-01 19:55:20 +0800

    added buttons

--- Digital_detox_app/app/src/main/java/com/example/digital_detox_app/MainActivity.kt
+++ Digital_detox_app/app/src/main/java/com/example/digital_detox_app/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.digital_detox_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.digital_detox_app.ui.theme.Digital_detox_appTheme
 
@@ -22,25 +27,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DigitalDetoxApp()
                 }
             }
         }
     }
 }
+@Composable
+fun DigitalDetoxApp() {
+    AppMainMenu(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppMainMenu(modifier: Modifier = Modifier) {
+    Column (
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.start_detox_session))
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.view_leaderboard))
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.view_analysis))
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.set_goal))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Digital_detox_appTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            DigitalDetoxApp()
+        }
     }
 }
