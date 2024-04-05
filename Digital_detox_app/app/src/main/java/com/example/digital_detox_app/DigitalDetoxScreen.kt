9521b08d69f071e1244bc73a9e20a64d7e45c011
john <ypoh007@e.ntu.edu.sg>
2024-04-05 13:10:49 +0800

    implemented Navigation between Pages. added main menu image.

---
+++ Digital_detox_app/app/src/main/java/com/example/digital_detox_app/DigitalDetoxScreen.kt
@@ -0,0 +1,118 @@
+package com.example.digital_detox_app
+
+import MainMenuScreen
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+enum class DigitalDetoxScreen(@StringRes val title: Int) {
+    MainMenu(title = R.string.app_name),
+    RunningSession(title = R.string.start_detox_session),
+    LeaderBoard(title = R.string.view_leaderboard),
+    Analysis(title = R.string.view_analysis),
+    SetGoal(title = R.string.set_goal)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DigitalDetoxBar (
+    currentScreen: DigitalDetoxScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun DigitalDetoxApp(
+//    viewModel: ViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    // Get current back stack entry
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    // Get the name of the current screen
+    val currentScreen = DigitalDetoxScreen.valueOf(
+        backStackEntry?.destination?.route ?: DigitalDetoxScreen.MainMenu.name
+    )
+    Scaffold (
+        topBar = {
+            DigitalDetoxBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry !=null,
+                navigateUp = { navController.navigateUp() })
+        }
+    ) { innerPadding ->
+//        val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = DigitalDetoxScreen.MainMenu.name,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+        ) {
+            composable(route = DigitalDetoxScreen.MainMenu.name) {
+                MainMenuScreen(
+                    navController = navController,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center)
+                )
+            }
+            composable(route = DigitalDetoxScreen.RunningSession.name) {
+                // TODO: create Screen for RunningSession
+            }
+            composable(route = DigitalDetoxScreen.LeaderBoard.name) {
+                // TODO: create Screen for Leaderboard
+            }
+            composable(route = DigitalDetoxScreen.Analysis.name) {
+                // TODO: create Screen for Analysis
+            }
+            composable(route = DigitalDetoxScreen.SetGoal.name) {
+                // TODO: create Screen for Set Goals
+            }
+        }
+    }
+}
+
+
--- Digital_detox_app/app/src/main/java/com/example/digital_detox_app/MainMenuScreen.kt
+++ Digital_detox_app/app/src/main/java/com/example/digital_detox_app/MainMenuScreen.kt
@@ -1,12 +1,11 @@
-package com.example.digital_detox_app
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,22 +13,53 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.digital_detox_app.data.DataSource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.digital_detox_app.DigitalDetoxApp
+import com.example.digital_detox_app.DigitalDetoxScreen
+import com.example.digital_detox_app.R
 import com.example.digital_detox_app.ui.theme.Digital_detox_appTheme
 
 @Composable
-fun DigitalDetoxApp(
-//    viewModel: ViewModel = ViewModel(),
-//    navController: NavHostController = rememberNavController()
+fun MainMenuScreen(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier,
 ) {
-    AppMainMenu(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
-    )
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(R.drawable.mainmenuimage),
+            contentDescription = null
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            MainMenuButton(
+                labelResourceId = R.string.start_detox_session,
+                onClick = {navController.navigate(DigitalDetoxScreen.RunningSession.name)}
+            )
+            MainMenuButton(
+                labelResourceId = R.string.view_leaderboard,
+                onClick = {navController.navigate(DigitalDetoxScreen.LeaderBoard.name)}
+            )
+            MainMenuButton(
+                labelResourceId = R.string.view_analysis,
+                onClick = {navController.navigate(DigitalDetoxScreen.Analysis.name)}
+            )
+            MainMenuButton(
+                labelResourceId = R.string.set_goal,
+                onClick = {navController.navigate(DigitalDetoxScreen.SetGoal.name)}
+            )
+        }
+    }
 }
 
 @Composable
@@ -43,27 +73,6 @@ fun MainMenuButton(
         modifier = modifier.widthIn(min = 250.dp)
     ) {
         Text(stringResource(labelResourceId))
-    }
-}
-
-@Composable
-fun AppMainMenu(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            DataSource.mainMenuButtons.forEach { item ->
-                MainMenuButton(
-                    labelResourceId = item,
-                    onClick = {}
-                )
-            }
-        }
     }
 }
 
